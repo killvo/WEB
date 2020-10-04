bf687b033722660a081ac7c3d75cd55d757cd149
Vova Chumak <volodymyr.4umak@gmail.com>
2020-10-04 21:30:59 +0300

    Допрацювання файлу для звітів1
    На сторінці звіту для ЛР-2 були перейменовані
    кнопки. Написаний скрипт для відображення в
    блоці картинок. Видалені зайві файли та змінено
    структуру папок.

--- scripts.js
+++ scripts.js
@@ -9,3 +9,23 @@ function show(fileName) {
         $(".show_load").html(data);
     });
 }
+
+// Для ЛР №2
+$(document).ready(function() {
+    $(".show_load").append("<img class='img_elem'>");
+})
+
+function openImgByPath(imgPath) {
+    $(".img_elem").attr("src", imgPath);
+}
+
+$('select2').change(function() {
+    switch ($("#select2 option:selected").getVal()) {
+        case "0":
+            openImgByPath('images/precDiagram.jpg');
+        case "1":
+            openImgByPath('images/precDiagram.jpg');
+        case "2":
+            openImgByPath('images/precDiagram.jpg');
+    }
+})
